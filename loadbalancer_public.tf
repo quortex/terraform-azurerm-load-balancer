e72785d15784d27b9be113d2966baed8dcf4f789
YH <y.hounsounou@quortex.io>
2023-09-21 11:19:15 +0200

    fix typo on rules

--- loadbalancer_public.tf
+++ loadbalancer_public.tf
@@ -153,7 +153,7 @@ resource "azurerm_application_gateway" "public" {
       http_listener_name         = "${var.public_app_gateway_http_listener_name_prefix}-add${request_routing_rule.key}"
       backend_address_pool_name  = var.public_app_gateway_backend_address_pool_name
       backend_http_settings_name = var.public_app_gateway_http_setting_name
-      priority                   = contains(var.additional_dns_records_public, request_routing_rule.key) ? index(var.additional_dns_records_public, request_routing_rule.key) + 200 : 100
+      priority                   = contains(var.additional_dns_records_public, request_routing_rule.key) ? index(var.additional_dns_records_public, request_routing_rule.key) + 200 : 1000
 
     }
   }

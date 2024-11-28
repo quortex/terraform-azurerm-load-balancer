e28318bceef2058367f9f0a6d2eb899867e6d4ad
YH <y.hounsounou@quortex.io>
2024-11-28 11:49:47 +0100

    remove lockfile, update constraint version, update gitignore

--- versions.tf
+++ versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_version = ">= 0.12"
 
   required_providers {
-    azurerm = ">= 4.11.0"
+    azurerm = ">= 4.0.0"
     acme = {
       source  = "vancluever/acme"
       version = "~> 2.0"
